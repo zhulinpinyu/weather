62c2d5ff8625f90b42e930be256597283cded06e
zhulinpinyu <mulixiang199016@gmail.com>
2016-04-29 17:39:14 +0800

    add weather reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import {combineReducers} from 'redux'
+import reducerWeather from './reducer_weather'
 
 const rootReducer = combineReducers({
-    state: (state={}) => state
+    weather: reducerWeather
 })
 
 export default rootReducer
