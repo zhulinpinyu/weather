d02920c7eb662146e599dbbb2b5ca1220f3afecf
zhulinpinyu <mulixiang199016@gmail.com>
2016-04-29 09:07:29 +0800

    setup redux middle ware env

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {Provider} from 'react-redux'
+import {createStore, applyMiddleware} from 'redux'
 
 import App from './components/App'
 
-ReactDOM.render(<App />,document.querySelector('.container'))
+import reducers from './reducers'
+
+const createStoreWithMiddleware = applyMiddleware()(createStore)
+
+ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        <App />
+    </Provider>,
+    document.querySelector('.container'))
