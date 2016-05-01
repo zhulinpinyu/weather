3ec7db4ba62611f82098056eb089bc582f91251a
zhulinpinyu <mulixiang199016@gmail.com>
2016-05-01 22:38:19 +0800

    show response data

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -2,6 +2,15 @@ import React,{Component} from 'react'
 import {connect} from 'react-redux'
 
 class WeatherList extends Component{
+    renderWeatherList(cityData){
+        const name = cityData.city.name
+        return (
+            <tr key={name}>
+                <td>{name}</td>
+            </tr>
+        )
+    }
+
     render(){
         return (
             <table className="table table-border">
@@ -14,6 +23,7 @@ class WeatherList extends Component{
                     </tr>
                 </thead>
                 <tbody>
+                    {this.props.weather.map(this.renderWeatherList)}
                 </tbody>
             </table>
         )
